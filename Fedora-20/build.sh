31b3b799c1345ccb57f051d9bbc76bd49ba60329
Jerry Vonau <jvonau3@gmail.com>
2015-03-29 02:19:52 -0500

    pungi's kickstart file at stage 2

--- Fedora-20/build.sh
+++ Fedora-20/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
 set -e 
-NAME='F20XSCE'
-VER='alpha'
+ARCH='x86_64'
+VER='F20'
+NAME='$VERXSCE'
 BUGURL='https://github.com/jvonau/XSCE-builder/issues'
+
+ISOBASE=$VER/$ARCH/os
 
 # gather, createrepo, buildinstall
 pungi --nosource --force \
@@ -11,16 +14,16 @@ pungi --nosource --force \
     --ver="$VER" --bugurl="$BUGURL" \
     -G -C -B
 
-# now the contents for the ISO are at 
-ISOBASE=$VER/i386/os
+echo "contents for the ISO are at: $ISOBASE"
 
-# Inject KS, overwrite isolinux.cfg
+# Inject KS
 cp XSCE-F20.ks $ISOBASE/
+# overwrite isolinux.cfg
 cp isolinux.cfg $ISOBASE/isolinux/
 
 # Compile the iso
 pungi --nosource --force \
-    -c olpcxs-pungi.ks --name "$NAME" \
+    -c xsce-pungi.ks --name "$NAME" \
     --ver="$VER" --bugurl="$BUGURL" \
     -I
 
